6ffb090641e58c03e3b25587871324fee5b2a093
Guillermo Valencia <enreda@gmail.com>
2025-11-19 19:01:11 +0100

    Fixed deploy to vercel

--- src/server.js
+++ src/server.js
@@ -1,5 +1,27 @@
-const { serveHTTP } = require("stremio-addon-sdk");
+const { serveHTTP } = require('stremio-addon-sdk');
+const addonInterface = require('./addon');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const addonInterface = require("./addon");
-serveHTTP(addonInterface, { port: 7000 });
+const PORT = process.env.PORT || 7000;
 
+const landingPage = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
+
+serveHTTP(addonInterface, { port: PORT, static: '/static' }, (err, server) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+
+    const originalListener = server.listeners('request')[0];
+    server.removeAllListeners('request');
+    server.on('request', (req, res) => {
+        if (req.url === '/') {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(landingPage);
+            return;
+        }
+        originalListener(req, res);
+    });
+});
